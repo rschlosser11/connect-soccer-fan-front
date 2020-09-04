08135c8465da733d3266769e954eaca7c42e7bca
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-04 09:54:22 -0500

    format date time on comments

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Toast } from 'react-bootstrap'
+import moment from 'moment';
 
 function Comment (props) {
+    let date = moment(props.comment.created_at)
     return (
         <Toast>
             <Toast.Header>
                 <strong className='mr-auto'>{props.comment.user.username}</strong>
-                <small>{props.comment.created_at}</small>
+                <small>{`${date.format('MMM')} ${date.get('date')} ${date.format('h:mm A')}`}</small>
             </Toast.Header>
             <Toast.Body>{props.comment.text}</Toast.Body>
         </Toast>
