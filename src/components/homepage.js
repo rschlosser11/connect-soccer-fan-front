b45d6b45a7d17e915a52251e756b141aa218b939
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-04 13:06:13 -0500

    add images

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Image, Carousel } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
+import audience from '../img/audience-1866738_1920.jpg'
+import ball from '../img/soccer-ball.png'
 
 export default function homepage () {
     return (
         <Container>
-            <h1>Welcome to Soccer Fan Connect!</h1>
+            <Carousel>
+                <Carousel.Item>
+                    <img 
+                        className='d-block w-100'
+                        src={audience}
+                        alt='header'
+                    />
+                    <Carousel.Caption>
+                        <h1>Welcome to Soccer Fan Connect</h1>
+                        <h5>A place to connect with fellow fans in the midst of the pandemic.</h5>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
             <p>Since the pandemic, many sporting events have been played behind closed doors and, while fans can usually watch from their homes, the experience is not the same. There's no connecting with fellow fans at a match or in a pub. No way to celebrate victories together or commiserate loses. Now there's Soccer Fan Connect!</p>
             <p>Soccer Fan Connect allows you to begin a chat on a live game, to connect with other fans and get a little bit of the match-day experience back.</p>
             <p>You must <Link to='/signup'>sign up</Link> to participate in the conversation. Enjoy the beautiful game!</p>
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Nav, Navbar, NavItem } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
+import ball from '../img/soccer-ball.png'
 
 export default function NavBar(props) {
     const handleLogout = () => {
@@ -12,7 +13,16 @@ export default function NavBar(props) {
     return(
         <Navbar bg='light' expand='lg'>
             <Navbar.Brand>
-                <Link className='nav-link' to='/'>Soccer Fan Connect</Link>
+                <Link className='nav-link' to='/'>
+                    <img 
+                        src={ball}
+                        width='30'
+                        height='30'
+                        className='d-inline-block align-top'
+                        alt='soccer ball' 
+                    />
+                    Soccer Fan Connect
+                </Link>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id='basic-navbar-nav'>
