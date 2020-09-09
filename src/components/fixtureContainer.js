c1e3e8ba9719e71c0d17f3620461b450fde031d6
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-09 08:55:58 -0500

    add Container to control width, add bottom margin to navbar

--- src/components/fixtureContainer.js
+++ src/components/fixtureContainer.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import Fixture from './fixture';
-import { Table } from 'react-bootstrap'
+import { Container, Table } from 'react-bootstrap'
 
 export default function FixturesContainer (props) {
     let fixtures = props.fixtures;
     return (
-        <Table striped bordered hover className='fixtures-table'>
-            <thead>
-                <tr>
-                    <th>Time/Result</th>
-                    <th>Home</th>
-                    <th>Away</th>
-                    <th>Venue</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {fixtures.map(fixture => <Fixture className='fixture' key={fixture.fixture_id} fixture={fixture} />)}
-            </tbody>
-            
-        </Table>
+        <Container>
+            <Table striped bordered hover className='fixtures-table'>
+                <thead>
+                    <tr>
+                        <th>Time/Result</th>
+                        <th>Home</th>
+                        <th>Away</th>
+                        <th>Venue</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {fixtures.map(fixture => <Fixture className='fixture' key={fixture.fixture_id} fixture={fixture} />)}
+                </tbody>
+            </Table>
+        </Container>
     )
 }
--- src/components/teamsContainer.js
+++ src/components/teamsContainer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Team from './team';
+import { Container } from 'react-bootstrap';
 
 export default function TeamsContainer (props) {
     let teams = props.teams;
     return (
-        <div className='teams-container'>
-            {teams.map(team => <Team className='team' key={team.team_id} team={team} />)}
-        </div>
+        <Container>
+            <div className='teams-container'>
+                {teams.map(team => <Team className='team' key={team.team_id} team={team} />)}
+            </div>
+        </Container>
     )
 }
